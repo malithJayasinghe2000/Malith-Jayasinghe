602c6f351138b070f3746cc6b2552f932a58732e
Malith Jayasinghe <malith2jayasinghe@gmail.com>
2024-01-07 11:07:10 +0530

    add drum machine

--- src/data/constants.js
+++ src/data/constants.js
@@ -477,19 +477,19 @@ export const projects = [
     github: "https://github.com/malithJayasinghe2000/Online-billing-and-event-reminder.git",
     webapp: "",
   },
-  // {
-  //   id: 5,
-  //   title: "Todo Web App",
-  //   date: "Jun 2021",
-  //   description:
-  //     " A Todo Web App made with React JS, Redux, and Material UI. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the tasks and can create a new task. It has a calendar where users can see all the tasks on a particular date. It has a search bar where users can search for a particular task.",
-  //   image:
-  //     "https://camo.githubusercontent.com/a328255ad96f861f57d25096d28018ab2656c689a1456b0d145764009bed2d1a/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f74253230283938292e706e673f616c743d6d6564696126746f6b656e3d33643335646366322d626666322d343730382d393031632d343232383866383332386633",
-  //   tags: ["React Js", "Local Storage", "AWS Auth", "Node JS"],
-  //   category: "web app",
-  //   github: "https://github.com/rishavchanda/Todo-Web-App",
-  //   webapp: "https://rishav-react-todo.netlify.app/",
-  // },
+  {
+    id: 5,
+    title: "Drum Machine",
+    date: "Jan 2024",
+    description:
+      " This web application made as a project of freeCodeCamp ",
+    image:
+      "https://res.cloudinary.com/dfnfzolon/image/upload/v1704605781/drum_r9fdtx.png",
+    tags: ["React Js", "HTML", "CSS"],
+    category: "web app",
+    github: "https://codepen.io/malith-jayasinghe/pen/wvOGJee",
+    webapp: "https://codepen.io/malith-jayasinghe/full/wvOGJee",
+  },
   // {
   //   id: 6,
   //   title: "Breaking Bad",
