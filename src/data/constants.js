49d1cb1ee3d7f58841ffad51d661b6489f217e16
Malith Jayasinghe <malith2jayasinghe@gmail.com>
2024-01-10 01:06:31 +0530

    add promodoro clock

--- src/data/constants.js
+++ src/data/constants.js
@@ -482,27 +482,27 @@ export const projects = [
     title: "Drum Machine",
     date: "Jan 2024",
     description:
-      " This web application made as a project of freeCodeCamp ",
+      " This web application made as a project of freeCodeCamp. In this can do change the volume of drum, turn off and turn on the drum and change the sound of drum keypad",
     image:
       "https://res.cloudinary.com/dfnfzolon/image/upload/v1704605781/drum_r9fdtx.png",
     tags: ["React Js", "HTML", "CSS"],
     category: "web app",
-    github: "https://codepen.io/malith-jayasinghe/pen/wvOGJee",
+    github: "https://github.com/malithJayasinghe2000/Drum-machine.git",
     webapp: "https://codepen.io/malith-jayasinghe/full/wvOGJee",
   },
-  // {
-  //   id: 6,
-  //   title: "Breaking Bad",
-  //   date: "Jun 2021",
-  //   description:
-  //     "A simple react app that shows the characters of the famous TV series Breaking Bad. It uses the Breaking Bad API to fetch the data. It also has a search bar to search for a particular character.",
-  //   image:
-  //     "https://camo.githubusercontent.com/cd07010cbeb90cb1b43a5d6162784326aef02210ef7d41a0f9ae043b3e392378/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f7425323028313534292e706e673f616c743d6d6564696126746f6b656e3d65613439383630632d303435362d343333342d616435372d336239346663303333363263",
-  //   tags: ["React Js", "API", "Axios", "Node JS"],
-  //   category: "web app",
-  //   github: "https://github.com/rishavchanda/Breaking-Bad",
-  //   webapp: "https://breaking-bad-webapp.netlify.app",
-  // },
+  {
+    id: 6,
+    title: "Pomodoro Clock",
+    date: "Jun 2024",
+    description:
+      "Developed a Pomodoro Clock application using React, HTML, CSS, and JavaScript. This responsive timer adheres to specified user stories, passing rigorous testing and resolving React 18 incompatibilities. Key features include session and break length controls, real-time countdown display, seamless transitions, and audio feedback on timer completion.",
+    image:
+      "https://res.cloudinary.com/dfnfzolon/image/upload/v1704828485/pomodoro_j4eagt.png",
+    tags: ["React Js", "HTML", "CSS"],
+    category: "web app",
+    github: "https://github.com/malithJayasinghe2000/Promodoro-Clock.git",
+    webapp: "https://codepen.io/malith-jayasinghe/full/RwdRZzG",
+  },
   // {
   //   id: 7,
   //   title: "Quiz App",
